09d314a905525212cd60ee0c329fa322b185d2ad
Emily Miles <emilymiles193@gmail.com>
2023-02-22 15:09:52 -0500

    began javascript functions

--- javascript.js
+++ javascript.js
@@ -1,4 +1,18 @@
-document.getElementById(start-button).addEventListener("click", startQuiz);
-function startQuiz {
+startButton=document.getElementById(start-button).addEventListener("click", startQuiz);
+let countDown=60;
+const quizArray=[{id:"0", question:"Javascript is a/n _____ language?",
+ options:["Object-Oriented", "Object-based","Procedural", "None of the Above"], correct:"Object-Oriented" },
+{id:"1", question:"Which of the following methods can be used to display data in some form using Javascript?",
+ options:["document.write()","console.log()","window.alert()","All of the Above"], correct: "All of the above" },
+{id:"2", question:"What keyword is used to check whether a given property is valid or not?",
+ options:["in","is in", "exists", "lies"], correct:"in" }
+];
+function startQuiz() {
+
+}
+function nextQuestion(){
+
+}
+function choseAnswer(){
 
 }
