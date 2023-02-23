6264e340ab395ea6e49289f3e09be29071dacfe1
Emily Miles <emilymiles193@gmail.com>
2023-02-22 21:18:01 -0500

    worked on answer buttons

--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
+const quizArray=[{question:"Javascript is a/n _____ language?",
+ options:["Object-Oriented", "Object-based","Procedural", "None of the Above"], correct:"Object-Oriented" },
+{question:"Which of the following methods can be used to display data in some form using Javascript?",
+ options:["document.write()","console.log()","window.alert()","All of the Above"], correct: "All of the above" },
+{question:"What keyword is used to check whether a given property is valid or not?",
+ options:["in","is in", "exists", "lies"], correct:"in" }
+];
 const startButton=document.getElementById('start-button');
 const nextButton= document.getElementById('next-button');
 const restartButton= document.getElementById('restart-button');
@@ -6,23 +13,19 @@ const timer=document.getElementById('timer');
 const totalTime=document.getElementById('time-left');
 startButton.addEventListener('click', startQuiz);
 nextButton.addEventListener('click', nextQuestion);
+restartButton.addEventListener('click', startQuiz);
 let countDown=60;
-const quizArray=[{id:"0", question:"Javascript is a/n _____ language?",
- options:["Object-Oriented", "Object-based","Procedural", "None of the Above"], correct:"Object-Oriented" },
-{id:"1", question:"Which of the following methods can be used to display data in some form using Javascript?",
- options:["document.write()","console.log()","window.alert()","All of the Above"], correct: "All of the above" },
-{id:"2", question:"What keyword is used to check whether a given property is valid or not?",
- options:["in","is in", "exists", "lies"], correct:"in" }
-];
+let randomQuestions;
+let questionIndex;
+const questionElement=document.getElementById('questionDisplay');
+console.log(questionElement);
+const optionsElement=document.getElementById('answer-buttons');
+
 
 function startQuiz() {
     setInterval(updateCountDown, 1000)
-    quizArray.sort(() => Math.random()-0.5);
-    //randomly generate quiz options
-    for (let i of quizArray){
-        i.options.sort(() =>Math.random()-0.5);
-    }
-
+    randomQuestions=quizArray.sort(() => Math.random()-0.5);
+    questionIndex=0;
     startButton.classList.add('hide');
     restartButton.classList.remove("hide");
     nextButton.classList.remove('hide');
@@ -32,8 +35,18 @@ function startQuiz() {
 
 }
 function nextQuestion(){
+applyQuestion(randomQuestions[questionIndex])
 
-
+}
+function applyQuestion(question){
+questionElement.innerText=question.question;
+question.options.forEach(options => {
+    const createButton=document.createElement('button');
+    createButton.innerText=options.text;
+    buttonGrid.classList.add('answer-button');
+    optionsElement.appendChild(createButton);
+}
+)
 }
 function choseAnswer(){
 
