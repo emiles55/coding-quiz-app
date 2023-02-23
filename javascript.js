18a9be21d9edc4e8195f257596f373d463f8ed94
Emily Miles <emilymiles193@gmail.com>
2023-02-22 19:11:18 -0500

    continued with startQuiz function

--- javascript.js
+++ javascript.js
@@ -1,4 +1,10 @@
-startButton=document.getElementById(start-button).addEventListener("click", startQuiz);
+const startButton=document.getElementById('start-button');
+const nextButton= document.getElementById('next-button');
+const restartButton= document.getElementById('restart-button');
+const buttonGrid=document.getElementById('questionAndAnswers');
+const timer=document.getElementById('timer');
+startButton.addEventListener('click', startQuiz);
+nextButton.addEventListener('click', nextQuestion);
 let countDown=60;
 const quizArray=[{id:"0", question:"Javascript is a/n _____ language?",
  options:["Object-Oriented", "Object-based","Procedural", "None of the Above"], correct:"Object-Oriented" },
@@ -7,7 +13,13 @@ const quizArray=[{id:"0", question:"Javascript is a/n _____ language?",
 {id:"2", question:"What keyword is used to check whether a given property is valid or not?",
  options:["in","is in", "exists", "lies"], correct:"in" }
 ];
+
 function startQuiz() {
+    startButton.classList.add('hide');
+    restartButton.classList.remove("hide");
+    nextButton.classList.remove('hide');
+    buttonGrid.classList.remove('hide');
+    timer.classList.remove('hide');
 
 }
 function nextQuestion(){
